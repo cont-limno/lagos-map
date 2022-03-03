78b66188b576972875067839ebf23df6afabb5ef
Nicole J Smith <nicole.j.smith@gmail.com>
2022-03-03 12:39:29 -0500

    update .gitignore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+function isValidHttpUrl(string) {
+  let url;
+  
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;  
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+console.log(isValidHttpUrl("http://localhost:8000/data/extract_1000.csv"))
+console.log(isValidHttpUrl("../data/extract_1000.csv"))
+
 Papa.parse("http://localhost:8000/data/extract_1000.csv", {
   worker: true,
   download: true,
